16c7ba09663b583258561389249bc3a8c850b033
Maykel Arias Torres <hanibal920915@gmail.com>
2022-06-05 13:26:20 -0400

    Initial

---
+++ 02-pokemon-static/components/UI/Navbar.tsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (<>
+    <div style={
+      {
+        display: 'flex',
+        width: '100',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'start',
+        padding: "0 20px",
+        backgroundColor: 'red'
+      }
+    }>
+      <span>Navbar</span>
+    </div>
+  </>);
+};
+
+export default Navbar;
